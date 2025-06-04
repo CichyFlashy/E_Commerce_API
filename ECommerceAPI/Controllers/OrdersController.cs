5ef993b899b1de79986fd889b58bb008119de4e9
Mateusz Cichos <mateusz.cichos@edu.uekat.pl>
2025-06-04 12:00:18 +0200

    feat: Add controller for Orders

---
+++ ECommerceAPI/Controllers/OrdersController.cs
@@ -0,0 +1,107 @@
+﻿using ECommerceAPI.Data;
+using ECommerceAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceAPI.Controllers
+{
+    [ApiController]
+    [Route("/orders")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public OrdersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+ 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(o => o.OrderDetails)
+                    .ThenInclude(od => od.Product)
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.OrderDetails)
+                    .ThenInclude(od => od.Product)
+                .FirstOrDefaultAsync(o => o.ID == id);
+
+            if (order == null)
+                return NotFound();
+
+            return order;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(Order order)
+        {
+            foreach (var detail in order.OrderDetails)
+            {
+                var product = await _context.Products.FindAsync(detail.ProductID);
+                if (product == null) return BadRequest("Nieprawidłowy ProductID");
+
+                detail.OrderPrice = detail.Quantity * product.price;
+            }
+
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOrder), new { id = order.ID }, order);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrder(int id, Order updatedOrder)
+        {
+            if (id != updatedOrder.ID)
+                return BadRequest();
+
+            _context.Entry(updatedOrder).State = EntityState.Modified;
+
+            foreach (var detail in updatedOrder.OrderDetails)
+            {
+                var product = await _context.Products.FindAsync(detail.ProductID);
+                if (product == null) return BadRequest("Nieprawidłowy ProductID");
+
+                detail.OrderPrice = detail.Quantity * product.price;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Orders.Any(o => o.ID == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.OrderDetails)
+                .FirstOrDefaultAsync(o => o.ID == id);
+
+            if (order == null)
+                return NotFound();
+
+            _context.OrderDetails.RemoveRange(order.OrderDetails);
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
+++ ECommerceAPI/obj/Debug/net8.0/ECommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e39f8bbf209b192882d6501b0cae5694b75aaa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562b60ab9feadbc8b7c7113f70ef71366eab8283")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
